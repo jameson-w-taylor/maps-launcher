44c4e4927684c415abd4ee866904d44bc9381d75
Jameson Taylor <jameson@ionic.io>
2023-05-05 17:36:45 -0400

    chore(): update capacitor config values

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'maps-launcher',
+  appId: 'io.ionic.mapslauncher',
+  appName: 'Maps Launcher',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
 };
 
 export default config;
